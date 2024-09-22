e8783df186c6f6a2cfed1c69b42c4f649d5bec1c
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2024-09-22 16:13:31 -0300

    add trava

--- src/salva_agendamento_service.py
+++ src/salva_agendamento_service.py
@@ -2,6 +2,7 @@ import os
 import json
 import boto3
 import pickle
+from datetime import datetime
 from aws_lambda_powertools import Logger
 
 class SalvaAgendamentoService():
@@ -15,7 +16,7 @@ class SalvaAgendamentoService():
     def handle_agendamento(self, agendamento: dict) -> bool:
         crm_medico: str = agendamento["crm_medico"]
         horario: str = agendamento["horario"]
-        path_arquivo_trava_horario: str = f'{crm_medico}-{horario}'
+        path_arquivo_trava_horario: str = f'{crm_medico}-{self.__format_time_trava(horario)}'
 
         self.logger.info(f'Path arquivo trava {path_arquivo_trava_horario}')
         existeTravaMedicoEHorario = self.__existe_trava_medico_e_horario(self.bucket_name, path_arquivo_trava_horario)
@@ -26,6 +27,7 @@ class SalvaAgendamentoService():
             agendamento["status_agendamento"] = "Rejeitado"
             self.__envio_notificacao_email(agendamento, "email_paciente")
         else:
+            self.__inserir_trava_horario(agendamento, path_arquivo_trava_horario)
             self.__alterar_status_agendamento(agendamento['id'], "Confirmado")
             agendamento["status_agendamento"] = "Confirmado"
             self.__envio_notificacao_email(agendamento, email_para_envio = agendamento["email_paciente"])
@@ -83,3 +85,27 @@ class SalvaAgendamentoService():
         except Exception as e:
             print(f'Erro ao atualizar status {id} do S3: {str(e)}')
             return None
+        
+    def __inserir_trava_horario(self, agendamento, path_trava):
+        json_trava = {
+            'id': id,
+            'horario': agendamento["horario"],
+            'crm_medico': agendamento["crm_medico"],
+            'cpf_paciente': agendamento["cpf_paciente"]
+        }
+
+        self.logger.info(f'JSON trava agendamento {json_trava}')
+        pickled_obj = pickle.dumps(json_trava)
+
+        self.logger.info(f'Iniciando put object s3')
+        self.s3_client.put_object(
+            Bucket=self.bucket_name,
+            Key=f"{path_trava}.pkl",
+            Body=pickled_obj
+        )
+
+    def __format_time_trava(self, time_string):
+        dt = datetime.strptime(time_string, "%Y-%m-%dT%H:%M")
+        formatted_time = dt.strftime("%d-%m-%Y-%H-%M")
+        
+        return formatted_time
